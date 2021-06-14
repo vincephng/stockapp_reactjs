ac9e9a775194d0b8de0aada5f061514078731b29
Phuc Nguyen <vincephng@gmail.com>
2021-06-13 22:32:34 -0700

    Fix 403 page

--- client/src/pages/403ForbiddenPage.js
+++ client/src/pages/403ForbiddenPage.js
@@ -7,7 +7,7 @@ const ForbiddenPage =  () =>{
                 <div className="d-flex justify-content-center align-items-center" id="main">
                     <h1 className="mr-3 pr-3 align-top border-right inline-block align-content-center" id="status">403</h1>
                         <div className="inline-block align-middle">
-                            <h1 className="font-weight-normal lead" id="desc">Oops!!! Look like we are not authorized.</h1>
+                            <h1 className="font-weight-normal lead" id="desc">Oops!!! Look like you are not authorized.</h1>
                             <p className='reason'>Please comback and log-in</p><br/>
                             <button className='back-home-bnt' onClick={() =>(history.push('/')) }>Back to Home</button>
                         </div>
